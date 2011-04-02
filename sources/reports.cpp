13bb637b72619ca10539175faa0fbea4da0ee991
VasayXTX <VasayXTX@mail.ru>
2011-04-02 11:30:39 +1100

    Commit before pull

--- sources/reports.cpp
+++ sources/reports.cpp
@@ -143,3 +143,7 @@ QString RepSport::GetQuery()
            "ranks r ON s.coach_id = c.id AND s.rank_id = r.id WHERE c.id = " +
            QString::number(vecId[cbCoach->currentIndex()]) + ";";
 }
+
+/******************************* Sertification *******************************/
+
+
